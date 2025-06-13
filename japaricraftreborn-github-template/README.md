198f1e1850a19c8f02b728932c557cf06c92ef98
Taichoo2006 <ace20061204@gmail.com>
2025-06-13 11:18:43 +0900

    Add files via upload

---
+++ japaricraftreborn-github-template/README.md
@@ -0,0 +1,1 @@
+# Japaricraft Reborn - Fabric 1.21.5 MOD Template
---
+++ japaricraftreborn-github-template/src/main/java/com/example/japaricraftreborn/JaparicraftrebornMod.java
@@ -0,0 +1,12 @@
+package com.example.japaricraftreborn;
+
+import net.fabricmc.api.ModInitializer;
+
+public class JaparicraftrebornMod implements ModInitializer {
+    public static final String MOD_ID = "japaricraftreborn";
+
+    @Override
+    public void onInitialize() {
+        System.out.println("Japaricraft Reborn initialized!");
+    }
+}
